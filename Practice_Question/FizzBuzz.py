f7c9ee022fecee2b343f0ed989ad4678b6a2cbc2
aditya Vishwakarma <avishwakarmapc2004@gmail.com>
2026-07-10 12:49:01 +0530

    FizzBuzz.py

--- Practice_Question/FizzBuzz.py
+++ Practice_Question/FizzBuzz.py
@@ -1,9 +1,12 @@
 # FizzBuzz: Print numbers from 1 to 50. For multiples of 3 print "Fizz", for multiples of 5 print "Buzz", and for multiples of both print "FizzBuzz".
 
-for i in range(1,50):
-    if i%3==0:
-        print('Fizz')
-    elif i%5==0:
-        print('Buzz')
-    elif i%3==0 or i%5==0:
+for i in range(1, 51):
+    if i % 3 == 0 and i % 5 == 0:
         print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
+        
